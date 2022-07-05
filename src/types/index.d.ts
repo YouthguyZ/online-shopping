b8e1763df1a6f3052b9afb78a5d2cdcc67987d77
youngguyz <2281300996@qq.com>
2022-07-05 19:26:33 +0800

    feat:完成了头部分类导航渲染-二级分类渲染

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,9 +1,10 @@
-// 类型声明
+// 类型声明 
 // 接口
 export interface categoryItem{
   id:string
   name:string
   picture:string
+  children:categoryItem[]
 }
 export interface ApiRes<t>{
   code:string
--- src/views/Layout/components/app-header-nav.vue
+++ src/views/Layout/components/app-header-nav.vue
@@ -1,7 +1,23 @@
 <template>
    <ul class="app-header-nav">
       <li class="home"><RouterLink to="/">首页</RouterLink></li>
-      <li v-for="item in category.list" :key="item.id" ><router-link to="/">{{item.name}}</router-link></li>
+      <li v-for="item in category.list" :key="item.id" ><router-link to="/">{{item.name}}</router-link>
+       <!-- 新增++++++++ -->
+      <div class="layer">
+        <ul>
+          <li v-for="sub in item.children" :key="sub.id">
+            <a href="#">
+              <img
+                :src="sub.picture"
+                alt=""
+              />
+              <p>{{sub.name}}</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <!-- 新增++++++++ -->
+      </li>
       
     </ul>
 </template>
@@ -15,25 +31,72 @@
 
 <style scoped lang="less">
 .app-header-nav {
-    width: 820px;
-    display: flex;
-    padding-left: 40px;
-    position: relative;
-    z-index: 998;
-    li {
-      margin-right: 40px;
-      width: 38px;
-      text-align: center;
-      a {
-        font-size: 16px;
-        line-height: 32px;
-        height: 32px;
-        display: inline-block;
-          &:hover {
-            color: @xtxColor;
-            border-bottom: 1px solid @xtxColor;
-          }
+  width: 820px;
+  display: flex;
+  padding-left: 40px;
+  position: relative;
+  z-index: 998;
+  > li {
+    margin-right: 40px;
+    width: 38px;
+    text-align: center;
+    > a {
+      font-size: 16px;
+      line-height: 32px;
+      height: 32px;
+      display: inline-block;
+      &:hover {
+        color: @xtxColor;
+        border-bottom: 1px solid @xtxColor;
+      }
+    }
+    // 新增样式
+    &:hover {
+      > a {
+        color: @xtxColor;
+        border-bottom: 1px solid @xtxColor;
+      }
+      > .layer {
+        height: 132px;
+        opacity: 1;
       }
     }
   }
+}
+// 新增样式
+.layer {
+  width: 1240px;
+  background-color: #fff;
+  position: absolute;
+  left: -200px;
+  top: 56px;
+  height: 0;
+  overflow: hidden;
+  opacity: 0;
+  box-shadow: 0 0 5px #ccc;
+  transition: all 0.2s 0.1s;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 70px;
+    align-items: center;
+    height: 132px;
+    li {
+      width: 110px;
+      text-align: center;
+      img {
+        width: 60px;
+        height: 60px;
+      }
+      p {
+        padding-top: 10px;
+      }
+      &:hover {
+        p {
+          color: @xtxColor;
+        }
+      }
+    }
+  }
+}
 </style>
